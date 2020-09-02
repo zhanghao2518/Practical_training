743af21ab1aa0b0497d2ec46cf5934e782e8ffea
zhanghao <2411410566@qq.com>
2020-09-02 23:20:34 +0800

    2020/9/2

---
+++ practical/src/main/java/com/tju/practical/PracticalApplication.java
@@ -0,0 +1,13 @@
+package com.tju.practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalApplication.class, args);
+    }
+
+}
---
+++ practical/src/test/java/com/tju/practical/PracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tju.practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
